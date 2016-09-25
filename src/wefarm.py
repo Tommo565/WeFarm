e4d3d43fe500f1a9c46da94917b44a7587f458db
Rebecca Murphy <rebeccaroisin@gmail.com>
2016-09-25 14:28:12 +0100

    code runs

--- src/wefarm.py
+++ src/wefarm.py
@@ -15,19 +15,6 @@ from nltk.stem.porter import PorterStemmer
 
 warnings.filterwarnings('ignore', category=DeprecationWarning, message='.*use @default decorator instead.*')
 
-# set random seed
-random_seed = 135
-state = np.random.RandomState(random_seed)
-
-path = '../data/'    # Data Directory 
-out = '../Outputs/'  # Output Directory
-msg = 'messages.csv'                       # Input Dataset
-
-sample_size = 5000
-topics = 10
-
-	
-
 class Message(object):
     def __init__(self, thread_id, date_time, message_id, user_id, language, msg_type, msg_body):
         self.thread_id = thread_id
@@ -95,11 +82,11 @@ def messages_vectorizer(messages):
     text = [p_stemmer.stem(i) for i in text]     # Stemming (reducing words to their root)
     return text
 
-def model(data, state):
+def model(data, state, num_topics):
     data_dict = corpora.Dictionary(data)                       # Creates an id <-> term dictionary
     data_corpus = [data_dict.doc2bow(text) for text in data]     # convert tokenized documents into a document-term matrix
     data_model = gensim.models.ldamodel.LdaModel(data_corpus, 
-                                                   num_topics=topics, 
+                                                   num_topics=num_topics, 
                                                    id2word = data_dict,
                                                    passes=20,
                                                    random_state=state)        #  generate LDA model
@@ -111,57 +98,57 @@ def model(data, state):
     return data_model, data_corpus, data_dict
 
 def generate_topic_desc_csv(the_model, word_dict):
-	with open('../Outputs/topics.csv', 'wb') as csvfile:
-    mywriter = csv.writer(csvfile, delimiter=',',quotechar='"')
-    for t in range(10):
-        topic_words= ['T'+str(t)]    
-        for k,v in the_model.get_topic_terms(t,topn=25):
-            word = word_dict[k]
-            topic_words.append(word)        
-        mywriter.writerow(topic_words)
+    with open('../Outputs/topics.csv', 'wb') as csvfile:
+        mywriter = csv.writer(csvfile, delimiter=',',quotechar='"')
+        for t in range(10):
+            topic_words= ['T'+str(t)]    
+            for k,v in the_model.get_topic_terms(t,topn=25):
+                word = word_dict[k]
+                topic_words.append(word)        
+            mywriter.writerow(topic_words)
 
 def generate_users_desc_csv(grouped_user_ids,the_model,the_corpus,n_topics=10):
-	with open('../Outputs/users.csv', 'wb') as csvfile:    
-    mywriter = csv.writer(csvfile, delimiter=',',quotechar='"')
-    
-    for i in range(len(grouped_user_ids)):
-        user_id= grouped_user_ids[i]
-        scores= the_model.get_document_topics(the_corpus[i])
-        scores_dict={}
-        for score in scores:
-            scores_dict[score[0]]=score[1]
-        scores_arr=[]
-        for j in range(n_topics):
-            if j in scores_dict.keys():
-                scores_arr.append(scores_dict[j])               
-            else:
-                scores_arr.append(0.0)
-        output_arr=[]
-        output_arr.append(user_id)
-        for s in scores_arr:
-            output_arr.append(str(s))            
-        mywriter.writerow(output_arr)
+    with open('../Outputs/users.csv', 'wb') as csvfile:    
+        mywriter = csv.writer(csvfile, delimiter=',',quotechar='"')
+        
+        for i in range(len(grouped_user_ids)):
+            user_id= grouped_user_ids[i]
+            scores= the_model.get_document_topics(the_corpus[i])
+            scores_dict={}
+            for score in scores:
+                scores_dict[score[0]]=score[1]
+            scores_arr=[]
+            for j in range(n_topics):
+                if j in scores_dict.keys():
+                    scores_arr.append(scores_dict[j])               
+                else:
+                    scores_arr.append(0.0)
+            output_arr=[]
+            output_arr.append(user_id)
+            for s in scores_arr:
+                output_arr.append(str(s))            
+            mywriter.writerow(output_arr)
 
 def generate_message_predictions_csv(messages,):
-	with open('../Outputs/message_predictions.csv', 'wb') as csvfile:    
-    mywriter = csv.writer(csvfile, delimiter=',',quotechar='"')
-    
-    for message in messages:
-        scores = topic_scores_for_message(message)
+    with open('../Outputs/message_predictions.csv', 'wb') as csvfile:    
+        mywriter = csv.writer(csvfile, delimiter=',',quotechar='"')
         
-        scores_dict={}
-        for score in scores:
-            scores_dict[score[0]]=score[1]
-        scores_arr=[]
-        for j in range(10):
-            if j in scores_dict.keys():
-                scores_arr.append(scores_dict[j])
-            else:
-                scores_arr.append(0.0)
-        output_arr=[message.thread_id, dt.strftime(message.date_time, "%Y-%m-%d %H:%M:%S"), message.message_id, message.user_id, message.language, message.msg_type, message.msg_body]
-        for s in scores_arr:
-            output_arr.append(str(s))
-        mywriter.writerow(output_arr)
+        for message in messages:
+            scores = topic_scores_for_message(message)
+            
+            scores_dict={}
+            for score in scores:
+                scores_dict[score[0]]=score[1]
+            scores_arr=[]
+            for j in range(10):
+                if j in scores_dict.keys():
+                    scores_arr.append(scores_dict[j])
+                else:
+                    scores_arr.append(0.0)
+            output_arr=[message.thread_id, dt.strftime(message.date_time, "%Y-%m-%d %H:%M:%S"), message.message_id, message.user_id, message.language, message.msg_type, message.msg_body]
+            for s in scores_arr:
+                output_arr.append(str(s))
+            mywriter.writerow(output_arr)
 
 def top_users_for_topic(topics_to_users, t, max_val=50):
     scores = topics_to_users[t]
@@ -169,22 +156,22 @@ def top_users_for_topic(topics_to_users, t, max_val=50):
     sorted_scores.reverse()
     return sorted_scores[:max_val]
     
-def topic_scores_for_message(message):
+def topic_scores_for_message(message, word_dict, model):
     message_to_predict = messages_vectorizer([message])
     message_to_predict = word_dict.doc2bow(message_to_predict)
     scores = the_model.get_document_topics(message_to_predict)
     return scores
 
 def top_users_for_message(message, user_scores):
-    message_scores = topic_scores_for_message(message)
+    message_scores = topic_scores_for_message(message, word_dict, model)
     topic = max(message_scores, key=lambda item:item[1])[0]
     top_users = [x for (x, y) in top_users_for_topic(user_scores, topic)]
     return topic, top_users
 
-def questions_to_recommended_users(messages,grouped_messages,topics_to_users):
+def questions_to_recommended_users(messages,grouped_messages,topics_to_users, word_dict, model):
     for message in messages[:100]:
         if(message.msg_type=='Q'):
-            topic, users = top_users_for_message(message, topics_to_users)
+            topic, users = top_users_for_message(message, topics_to_users, word_dict, model)
             print 'Question: '
             print message.msg_body, topic
             
@@ -200,92 +187,107 @@ def questions_to_recommended_users(messages,grouped_messages,topics_to_users):
             print
 
 def topics_to_users_fn(grouped_user_ids,the_model,the_corpus,n_topics=10):
-	topics_to_users={}
-	for j in range(n_topics):
-	    topics_to_users[j]={}
-    for i in range(len(grouped_user_ids)):
-    user_id= grouped_user_ids[i]
-    scores= the_model.get_document_topics(the_corpus[i])
-    scores_dict={}
-    for score in scores:
-        scores_dict[score[0]]=score[1]
-    scores_arr=[]
+    topics_to_users={}
     for j in range(n_topics):
-        if j in scores_dict.keys():
-            topics_to_users[j][user_id]=scores_dict[j]                
-        else:
-            topics_to_users[j][user_id]=0.0
+        topics_to_users[j]={}
+    for i in range(len(grouped_user_ids)):
+        user_id= grouped_user_ids[i]
+        scores= the_model.get_document_topics(the_corpus[i])
+        scores_dict={}
+        for score in scores:
+            scores_dict[score[0]]=score[1]
+        scores_arr=[]
+        for j in range(n_topics):
+            if j in scores_dict.keys():
+                topics_to_users[j][user_id]=scores_dict[j]                
+            else:
+                topics_to_users[j][user_id]=0.0
     return topics_to_users
 
 def test_prediction():
-	# processing a message to make a prediction
-	users_topic_model = joblib.load('../Outputs/users_topic_model.pkl' ) 
+    # processing a message to make a prediction
+    users_topic_model = joblib.load('../Outputs/users_topic_model.pkl' ) 
 
-	the_model = users_topic_model[0]
-	the_corpus = users_topic_model[1]
-	word_dict = users_topic_model[2]
+    the_model = users_topic_model[0]
+    the_corpus = users_topic_model[1]
+    word_dict = users_topic_model[2]
 
-	print(the_corpus[0])
-	test_message = messages[0]
-	print(test_message.msg_body.lower())
-	the_model.get_document_topics(the_corpus[0])
-	test_message = messages_vectorizer([test_message])
-	print(test_message)
-	test_vectorized = word_dict.doc2bow(test_message)
+    print(the_corpus[0])
+    test_message = messages[0]
+    print(test_message.msg_body.lower())
+    the_model.get_document_topics(the_corpus[0])
+    test_message = messages_vectorizer([test_message])
+    print(test_message)
+    test_vectorized = word_dict.doc2bow(test_message)
 
-	the_model.get_document_topics(test_vectorized)
+    the_model.get_document_topics(test_vectorized)
 
-	for k, v in the_model.get_topic_terms(4, topn=25):
-	    word = word_dict[k]
-	    print(word)
+    for k, v in the_model.get_topic_terms(4, topn=25):
+        word = word_dict[k]
+        print(word)
 
 
 def main():
-	# Load data
-	df_msg_in = pd.read_csv(path + msg)
-	df_msg_en = df_msg_in[(df_msg_in['language'] == 'EN')] 
-	# Group by users
+    # set up parameters
+    # set random seed
+    random_seed = 135
+    state = np.random.RandomState(random_seed)
+
+    path = '../data/'    # Data Directory 
+    out = '../Outputs/'  # Output Directory
+    msg = 'messages.csv'                       # Input Dataset
+
+    sample_size = 5000
+    num_topics = 10
+    # Load data
+    print("Loading data")
+    df_msg_in = pd.read_csv(path + msg)
+    df_msg_en = df_msg_in[(df_msg_in['language'] == 'EN')] 
+    # Group by users
     users, messages = loadData(path + msg)
-	grouped_messages =  getMessageGroups(messages, 'user_id')
+    grouped_messages =  getMessageGroups(messages, 'user_id')
 
-	#preparing data to build the topic model
-	grouped_user_data =[]
+    #preparing data to build the topic model
+    print("Preparing data")
+    grouped_user_data =[]
 
-	min_messages = 3
-	max_messages = 300
+    min_messages = 3
+    max_messages = 300
 
-	grouped_messages_by_user = grouped_messages
-	for k in grouped_messages.keys():
-	    v = grouped_messages[k]
-	    if (len(v)<max_messages and len(v)>min_messages):
-	        grouped_user_data.append(messages_vectorizer(v))
+    grouped_messages_by_user = grouped_messages
+    for k in grouped_messages.keys():
+        v = grouped_messages[k]
+        if (len(v)<max_messages and len(v)>min_messages):
+            grouped_user_data.append(messages_vectorizer(v))
 
-	grouped_user_ids=[]
-	for k in grouped_messages.keys():
-	    v = grouped_messages[k]
-	    if (len(v)<max_messages and len(v)>min_messages):
-	        grouped_user_ids.append(k)
+    grouped_user_ids=[]
+    for k in grouped_messages.keys():
+        v = grouped_messages[k]
+        if (len(v)<max_messages and len(v)>min_messages):
+            grouped_user_ids.append(k)
 
-	# build the topic model and save it
-	users_topic_model = model(grouped_user_data, state)
-	the_model = users_topic_model[0]
-	the_corpus = users_topic_model[1]
-	word_dict = users_topic_model[2]
-	joblib.dump(users_topic_model, '../Outputs/users_topic_model.pkl' ) 
+    # build the topic model and save it
+    print("Building the model")
+    users_topic_model = model(grouped_user_data, state, num_topics)
+    the_model = users_topic_model[0]
+    the_corpus = users_topic_model[1]
+    word_dict = users_topic_model[2]
+    joblib.dump(users_topic_model, '../Outputs/users_topic_model.pkl' ) 
 
-	# load the model that have just been built
-	users_topic_model = joblib.load('../Outputs/users_topic_model.pkl')
-	the_model = users_topic_model[0]
-	the_corpus = users_topic_model[1]
-	word_dict = users_topic_model[2]
+    # load the model that have just been built
+    users_topic_model = joblib.load('../Outputs/users_topic_model.pkl')
+    the_model = users_topic_model[0]
+    the_corpus = users_topic_model[1]
+    word_dict = users_topic_model[2]
 
-	generate_topic_desc_csv(the_model, word_dict)
+    print("Write topic csv")
+    generate_topic_desc_csv(the_model, word_dict)
 
-	topics_to_users = topics_to_users_fn(grouped_user_ids,the_model,the_corpus)
+    topics_to_users = topics_to_users_fn(grouped_user_ids,the_model,the_corpus)
 
-
-	questions_to_recommended_users(messages,grouped_messages,topics_to_users)
-	
+    print("Generate users for questions")
+    questions_to_recommended_users(messages,grouped_messages,topics_to_users, word_dict, the_model)
+    
 
 
 if __name__ == "__main__": main()
